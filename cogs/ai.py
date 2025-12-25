02561f71690d41d4946e7f61b2b3ad2eaf041d1b
cltq <shapebrabra@gmail.com>
2025-12-26 00:51:40 +0700

    fix ask command doesnt applied to user install

--- cogs/ai.py
+++ cogs/ai.py
@@ -315,7 +315,9 @@ class AI(commands.Cog):
         save_config(config)
         
         await interaction.response.send_message(f"✅ ลบการตั้งค่าห้องนี้เรียบร้อยแล้ว")
-
+        
+    @discord.app_commands.allowed_installs(guilds=True, users=True)
+    @discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
     @app_commands.command(name="ask", description="ถามคำถาม AI พร้อมกำหนดบุคลิก/คำสั่งเบื้องต้น")
     @app_commands.describe(
         question="คำถามที่คุณต้องการถาม",
